cea1f4fd86c8d500b13beb5ed5b8748905191ba7
arjun-ms <arjunachu123.aa@gmail.com>
2025-05-06 14:30:36 +0530

    GET endpoint to retrieve a Todo by ID

--- todo/main.py
+++ todo/main.py
@@ -18,7 +18,7 @@ def get_db():
         db.close()
 
 
-
+# POST endpoint to create a new Todo
 @app.post("/todo")
 def create(request: schemas.Todo, db: Session = Depends(get_db)):
     # Create a new Todo model instance with the data from the request
@@ -33,7 +33,16 @@ def create(request: schemas.Todo, db: Session = Depends(get_db)):
     
     return new_todo
 
+# GET endpoint to retrieve all Todos
 @app.get("/todo")
 def get_all(db: Session = Depends(get_db)):
     todos = db.query(models.Todo).all()
     return todos
+
+# GET endpoint to retrieve a Todo by ID
+@app.get("/todo/{id}")
+def get_todo(id: int, db: Session = Depends(get_db)):
+    todo = db.query(models.Todo).filter(models.Todo.id == id).first()
+    if not todo:
+        return {"error": "Todo not found"}
+    return todo
